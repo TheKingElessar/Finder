c96b80b99864c222c8b7a86fae905ebd1b904cb7
Nathan Panzer <wasabimath@gmail.com>
2021-11-29 18:53:25 -0700

    ?

---
+++ main.py
@@ -0,0 +1,180 @@
+import re
+from pathlib import Path
+
+import imdb
+import csv
+
+import re
+
+
+def progress_bar(current, total, bar_length=20):
+    percent = float(current) * 100 / total
+    arrow = '█' * int(percent / 100 * bar_length - 1) + '○'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
+
+
+def count_syllables(word):
+    word = word.lower()
+
+    # exception_add are words that need extra syllables
+    # exception_del are words that need less syllables
+
+    exception_add = ['serious', 'crucial']
+    exception_del = ['fortunately', 'unfortunately']
+
+    co_one = ['cool', 'coach', 'coat', 'coal', 'count', 'coin', 'coarse', 'coup', 'coif', 'cook', 'coign', 'coiffe',
+              'coof', 'court']
+    co_two = ['coapt', 'coed', 'coinci']
+
+    pre_one = ['preach']
+
+    syls = 0  # added syllable number
+    disc = 0  # discarded syllable number
+
+    # 1) if letters < 3 : return 1
+    if len(word) <= 3:
+        syls = 1
+        return syls
+
+    # 2) if doesn't end with "ted" or "tes" or "ses" or "ied" or "ies", discard "es" and "ed" at the end.
+    # if it has only 1 vowel or 1 set of consecutive vowels, discard. (like "speed", "fled" etc.)
+
+    if word[-2:] == "es" or word[-2:] == "ed":
+        doubleAndtripple_1 = len(re.findall(r'[eaoui][eaoui]', word))
+        if doubleAndtripple_1 > 1 or len(re.findall(r'[eaoui][^eaoui]', word)) > 1:
+            if word[-3:] == "ted" or word[-3:] == "tes" or word[-3:] == "ses" or word[-3:] == "ied" or word[
+                                                                                                       -3:] == "ies":
+                pass
+            else:
+                disc += 1
+
+    # 3) discard trailing "e", except where ending is "le"
+
+    le_except = ['whole', 'mobile', 'pole', 'male', 'female', 'hale', 'pale', 'tale', 'sale', 'aisle', 'whale', 'while']
+
+    if word[-1:] == "e":
+        if word[-2:] == "le" and word not in le_except:
+            pass
+
+        else:
+            disc += 1
+
+    # 4) check if consecutive vowels exists, triplets or pairs, count them as one.
+
+    doubleAndtripple = len(re.findall(r'[eaoui][eaoui]', word))
+    tripple = len(re.findall(r'[eaoui][eaoui][eaoui]', word))
+    disc += doubleAndtripple + tripple
+
+    # 5) count remaining vowels in word.
+    numVowels = len(re.findall(r'[eaoui]', word))
+
+    # 6) add one if starts with "mc"
+    if word[:2] == "mc":
+        syls += 1
+
+    # 7) add one if ends with "y" but is not surrouned by vowel
+    if word[-1:] == "y" and word[-2] not in "aeoui":
+        syls += 1
+
+    # 8) add one if "y" is surrounded by non-vowels and is not in the last word.
+
+    for i, j in enumerate(word):
+        if j == "y":
+            if (i != 0) and (i != len(word) - 1):
+                if word[i - 1] not in "aeoui" and word[i + 1] not in "aeoui":
+                    syls += 1
+
+    # 9) if starts with "tri-" or "bi-" and is followed by a vowel, add one.
+
+    if word[:3] == "tri" and word[3] in "aeoui":
+        syls += 1
+
+    if word[:2] == "bi" and word[2] in "aeoui":
+        syls += 1
+
+    # 10) if ends with "-ian", should be counted as two syllables, except for "-tian" and "-cian"
+
+    if word[-3:] == "ian":
+        # and (word[-4:] != "cian" or word[-4:] != "tian") :
+        if word[-4:] == "cian" or word[-4:] == "tian":
+            pass
+        else:
+            syls += 1
+
+    # 11) if starts with "co-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
+
+    if word[:2] == "co" and word[2] in 'eaoui':
+
+        if word[:4] in co_two or word[:5] in co_two or word[:6] in co_two:
+            syls += 1
+        elif word[:4] in co_one or word[:5] in co_one or word[:6] in co_one:
+            pass
+        else:
+            syls += 1
+
+    # 12) if starts with "pre-" and is followed by a vowel, check if exists in the double syllable dictionary, if not, check if in single dictionary and act accordingly.
+
+    if word[:3] == "pre" and word[3] in 'eaoui':
+        if word[:6] in pre_one:
+            pass
+        else:
+            syls += 1
+
+    # 13) check for "-n't" and cross match with dictionary to add syllable.
+
+    negative = ["doesn't", "isn't", "shouldn't", "couldn't", "wouldn't"]
+
+    if word[-3:] == "n't":
+        if word in negative:
+            syls += 1
+        else:
+            pass
+
+            # 14) Handling the exceptional words.
+
+    if word in exception_del:
+        disc += 1
+
+    if word in exception_add:
+        syls += 1
+
+        # calculate the output
+    return numVowels - disc + syls
+
+
+ia = imdb.IMDb()
+
+output_file = Path("found.txt")
+
+with open('../../Downloads/ml-latest/ml-latest/movies.csv', mode='r') as file:
+    csvFile = csv.reader(file)
+    skipped = False
+    movie_num = 0
+    for lines in csvFile:
+        if not skipped:
+            skipped = True
+            continue
+        movie_num = lines[0]
+        movie = lines[1]
+        pattern = re.compile('(?P<title1>.*?)(?:(, )(?P<title2>.*))? (?P<year>\([0-9]*\))\"?')
+        if re.match(pattern, movie):
+            match = re.match(pattern, movie)
+            if match.groupdict()["title2"]:
+                title = match.groupdict()["title2"] + " " + match.groupdict()["title1"]
+            else:
+                title = match.groupdict()["title1"]
+
+            year = match.groupdict()["year"]
+
+            results = ia.search_movie(title + " " + year)
+            if len(results) == 0:
+                continue
+            result = results[0]
+            result_title = result["title"]
+            if count_syllables(result_title) == 2:
+                if result_title[-2:] == "er":
+                    with output_file.open("a") as f:
+                        f.write(result_title + f" ({result['year']})" + "\n")
+                    progress_bar(movie_num, 193886)
